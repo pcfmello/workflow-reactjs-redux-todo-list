5cfc1ef28be40fe83e730eab9c1938eebb00ea26
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2018-06-24 19:52:39 -0300

    Isolate the system in many components M3A42

--- src/app.js
+++ src/app.js
@@ -1,55 +1,17 @@
 "use strict";
 
 import React from "react";
-import { connect } from "react-redux";
-import {
-  addTodo,
-  toggleTodo
-} from "./redux-flow/reducers/todos/action-creators";
 
-const App = ({ todos, handleAddTodo, handleToggleTodo }) => (
+import Form from "./redux-flow/components/Form";
+import TodosList from "./redux-flow/components/TodosList";
+import Filter from "./redux-flow/components/Filter";
+
+const App = () => (
   <div>
-    <form onSubmit={handleAddTodo}>
-      <input type="text" name="todo" />
-      <button type="submit">Adicionar</button>
-    </form>
-
-    <ul>
-      {todos.map(todo => (
-        <li
-          key={todo.id}
-          style={{ textDecoration: todo.completed ? "line-through" : "none" }}
-          onClick={handleToggleTodo(todo.id)}
-        >
-          {todo.text}
-        </li>
-      ))}
-      {!todos.length && <li>Não existem tarefas nessa lista</li>}
-    </ul>
-
-    <div>
-      <h3>Mostrar</h3>
-      <span>Todos</span> | <a href="">Finalizados</a> | <a href="">A fazer</a>
-    </div>
+    <Form />
+    <TodosList />
+    <Filter />
   </div>
 );
 
-const mapStateToProps = state => ({
-  todos: state.todos
-});
-
-const mapDispatchToProps = dispatch => ({
-  handleAddTodo: e => {
-    e.preventDefault();
-    dispatch(addTodo(e.target.todo.value));
-    e.target.todo.value = "";
-  },
-  handleToggleTodo: id => e => {
-    dispatch(toggleTodo(id));
-  }
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default App;
---
+++ src/redux-flow/components/TodosList/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import { toggleTodo } from "../../reducers/todos/action-creators";
+
+const TodosList = ({ todos, handleToggleTodo }) => (
+  <ul>
+    {todos.map(todo => (
+      <li
+        key={todo.id}
+        style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+        onClick={handleToggleTodo(todo.id)}
+      >
+        {todo.text}
+      </li>
+    ))}
+    {!todos.length && <li>Não existem tarefas nessa lista</li>}
+  </ul>
+);
+
+const mapStateToProps = state => ({
+  todos: state.todos
+});
+
+const mapDispatchToProps = dispatch => ({
+  handleToggleTodo: id => e => {
+    dispatch(toggleTodo(id));
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodosList);
