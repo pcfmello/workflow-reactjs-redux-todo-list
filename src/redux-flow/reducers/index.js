7ee5ae03a197041040ec630dcb51e2c835995704
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2018-06-24 18:40:42 -0300

    Learning as working the combineReducer M3A41

--- src/redux-flow/reducers/index.js
+++ src/redux-flow/reducers/index.js
@@ -5,6 +5,23 @@ import { combineReducers } from "redux";
 import todos from "./todos";
 import visibilityFilter from "./visibility-filter";
 
+// Exemplo simples do funcionamento do combineReducers por baixo dos panos
+/* const combineReducers = reducers => (state = {}, action) => {
+  return Object.keys(reducers).reduce((nextState, key) => {
+    nextState[key] = reducers[key](state[key], action);
+    return nextState;
+  }, {});
+}; */
+// OU
+/* const combineReducers = reducers => (state = {}, action) => {
+  return Object.keys(reducers).reduce((nextState, key) => {
+    return {
+      ...nextState,
+      [key]: reducers[key](state[key], action);
+    }
+  }, {});
+}; */
+
 /**
  * combineReducers
  * about: Função do Redux que combina os vários reducers da aplicação
